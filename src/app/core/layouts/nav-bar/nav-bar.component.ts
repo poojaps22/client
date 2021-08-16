c6821eba5b986fb7ecbc9ce23c2c9d553ef1ff78
poojaps22 <poo.pspramesh@gmail.com>
2021-08-16 17:49:01 +0530

    updated

--- src/app/core/layouts/nav-bar/nav-bar.component.ts
+++ src/app/core/layouts/nav-bar/nav-bar.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/users/services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
-
-  constructor() { }
+  username: string;
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.authService.subject.subscribe((res) => {
+      this.username = res;
+    });
   }
-
 }
